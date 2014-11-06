378c692fe8055911f36dff02bfe152df228df065
Rich Kourtz <rkourtz@nuodb.com>
2014-11-06 16:42:14 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup
 import sys
 
-#v = sys.version_info
-#if v[0] == 2 and v[1] == 7:
 setup(name='nuodbawsquickstart',
-      version='0.1.0',
+      version='1.0.0',
       description='Script to deploy a multi-region and multi-instance AWS cluster',
       url='http://github.com/nuodb/nuodb-aws-quickstart',
       author='NuoDB Inc.',
@@ -15,6 +13,3 @@ setup(name='nuodbawsquickstart',
       packages=['nuodbawsquickstart'],
       scripts=["nuodb_aws_quickstart.py"],
       zip_safe=False)
-#else:
-#  print "This module and some of its dependencies only work on Python version 2.7. Detected %s. Cannot continue." % ".".join(str(e) for e in v[0:2])
-#  exit(2)
